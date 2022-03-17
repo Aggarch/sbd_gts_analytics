5eacd1a53d9a4151006cc7ff091b5bbd6aaa12ef
Garcia <andresgarcia.dc@gmail.com>
2022-03-17 11:24:15 -0500

    FX

--- rates_fx.R
+++ rates_fx.R
@@ -7,9 +7,11 @@ model   <- "C:/Users/AEG1130/Documents/fx"
 docs    <- "C:/Users/AEG1130/Documents" 
 
 library(tidyverse)
+library(openxlsx)
 
 # Rates & Keep Bzns Days
 
+SBD_FX         <- function(){ 
 
 # Extraction funciton ::::::::::::::::::::::
 
@@ -63,18 +65,6 @@ resource <- rate_files %>%
   filter(!is.na(hfm_day))
 
 
-# FX Construct 
-
-setwd(model)
-
-order.cur <- openxlsx::read.xlsx("GTS_FX_2022_FEB28_SPOT_03022022@7PM.xlsx",
-                    sheet = "SBD Rates 2022")
-
-struct <- order.cur %>% as_tibble() %>% select(X2,X3) %>% filter(!is.na(X2)) %>% 
-  rename(from_ccy = X2,
-         name = X3)
-
-
 # Extractor
 
 setwd(path)
@@ -107,9 +97,6 @@ last_info_date<- pull_long %>%
   filter(date == max(date)) %>% distinct(date)
 
 
-
-
-
 return(list(table = pull_long,
             last_date = last_info_date))
 
@@ -123,11 +110,25 @@ last  <- rates_table(fx_last)
 
 
 # Merged Cleaned Data (FX RATES) :::::::::::
+# Equivalent to the daily_rates sheet on SBD_Rates.xlsx
+
 pull_long <- table$table %>%
   bind_rows(last$table)
 
 
-original_sbd_rates <- function(){ 
+return(pull_long)
+
+}
+
+pull_long      <- SBD_FX()
+
+
+
+
+# Original Perspective, wider pivot SBD_Rates sheet
+# With Excel Output
+
+SBD_Rates_original <- function(){ 
   
 # Wider Pivot ::::::::::::::::::::::::::::::
 wider_pull <- pull_long %>% select(from_ccy, date, inverse_spot_rate) %>% 
@@ -135,57 +136,90 @@ wider_pull <- pull_long %>% select(from_ccy, date, inverse_spot_rate) %>%
   
 
 # Verify desire order - - - - -- - -- -- - - -
+# FX Construct 
+
 model   <- "C:/Users/AEG1130/Documents/fx"   
 setwd(model)
 
+order.cur <- openxlsx::read.xlsx("GTS_FX_2022_FEB28_SPOT_03022022@7PM.xlsx",
+                                 sheet = "SBD Rates 2022")
+
+struct <- order.cur %>% as_tibble() %>% select(X2,X3) %>% filter(!is.na(X2)) %>% 
+  rename(from_ccy = X2,
+         currency = X3)
+
+
+wider_pull <- struct %>% left_join(wider_pull, by = "from_ccy")
+
 
 # Save Wider Pivot - - - - - - - - - - - - - -
-struct %>% left_join(wider_pull, by = "from_ccy") %>% 
-  openxlsx::write.xlsx("rates_fx.xlsx", overwrite = T)
+
+wider_pull %>% openxlsx::write.xlsx("SBD_Rates_original.xlsx", overwrite = T)
+
+return(wider_pull)
 
 }
 
+
+
+
 # Querying, to be able plotting --------------
-
-# Examples time windows 
-
-time_windows <- function(){ 
-
-w_avgs <- pull_long %>% 
-  mutate(date = as.Date(date, origin = "1899-12-30")) %>% 
-  group_by(from_ccy, week, month,year) %>%
-  summarise(n_periods_per_week = n(),
-            last_date = max(date),
-            first_date = min(date),
-            average = sum(inverse_spot_rate)/n_periods_per_week,
-            .groups = "drop") %>% 
-  mutate(date_range = paste(first_date ,":", last_date)) %>% 
-  mutate(week = str_replace_all(week,"Week","")) %>% 
-  mutate(week = str_trim(week)) %>% 
-  mutate(week = as.numeric(week)) %>% 
-  arrange(week) %>% 
-  select(-last_date, -first_date)
+# Examples time windows : Excel output
 
 
-m_avgs <- pull_long %>% 
-  mutate(date = as.Date(date, origin = "1899-12-30")) %>%
-    group_by(from_ccy,month) %>% summarise(n_periods = n(),
-                                           idate = min(date),
-                                           fdate = max(date),
-                                           iweek = first(week),
-                                           fweek = last(week),
-                                           avg = sum(inverse_spot_rate)/n_periods,
-                                           .groups = "drop") %>% 
-  mutate(date_range = paste(idate,":",fdate)) %>% 
-  mutate(week_range = paste(iweek,":",fweek)) %>% 
-  mutate(month.n = lubridate::month(idate)) %>% 
-    select(-idate, -fdate,
-           -iweek, -fweek) %>% 
-    arrange(month.n)
+SBD_Rates <- function(){ 
+
+  wb <- createWorkbook()
+  addWorksheet(wb,"daily_data")
+  addWorksheet(wb,"weekly_rates")
+  addWorksheet(wb,"monthly_rates")
+  
+ daily_data <- pull_long  
+  
+  weekly_rates <- daily_data %>% 
+      mutate(date = as.Date(date, origin = "1899-12-30")) %>% 
+      group_by(from_ccy, week, month,year) %>%
+      summarise(n_periods_per_week = n(),
+                last_date = max(date),
+                first_date = min(date),
+                average = sum(inverse_spot_rate)/n_periods_per_week,
+                .groups = "drop") %>% 
+      mutate(date_range = paste(first_date ,":", last_date)) %>% 
+      mutate(week = str_replace_all(week,"Week","")) %>% 
+      mutate(week = str_trim(week)) %>% 
+      mutate(week = as.numeric(week)) %>% 
+      arrange(week) %>% 
+      select(-last_date, -first_date)
 
 
-return(list(daily_rates   =   pull_long,
-            weekly_rates  =  w_avgs,
-            monthly_rates = m_avgs))
+ monthly_rates <- daily_data %>% 
+      mutate(date = as.Date(date, origin = "1899-12-30")) %>%
+        group_by(from_ccy,month) %>% summarise(n_periods = n(),
+                                               idate = min(date),
+                                               fdate = max(date),
+                                               iweek = first(week),
+                                               fweek = last(week),
+                                               avg = sum(inverse_spot_rate)/n_periods,
+                                               .groups = "drop") %>% 
+      mutate(date_range = paste(idate,":",fdate)) %>% 
+      mutate(week_range = paste(iweek,":",fweek)) %>% 
+      mutate(month.n = lubridate::month(idate)) %>% 
+        select(-idate, -fdate,
+               -iweek, -fweek) %>% 
+        arrange(month.n)
+    
+
+
+  writeData(wb, "daily_data", daily_data)
+  writeData(wb, "weekly_rates", weekly_rates)
+  writeData(wb, "monthly_rates", monthly_rates)
+
+  
+  openxlsx::saveWorkbook(wb, file = "SBD_Rates.xlsx", overwrite = TRUE)
+  
+
+return(list(daily_rates   =  daily_data,
+            weekly_rates  =  weekly_rates,
+            monthly_rates =  monthly_rates))
 
 }
