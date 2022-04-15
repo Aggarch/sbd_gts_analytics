c940c5e6cc4cfd796e1da710b27e489e777caa96
Garcia <Andres.Garcia@sbdinc.com>
2022-04-15 18:40:44 -0400

    dimensions

--- dimensions_data.R
+++ dimensions_data.R
@@ -13,6 +13,9 @@ setwd(dimensions)
   as_tibble() %>% 
   mutate(key = str_replace_all(value, ".xlsx","")) %>% 
   mutate(key = str_replace_all(key, "_GTS_GEM","")) %>% 
+  mutate(key = str_replace_all(key, "~$","")) %>% 
+  mutate(value = str_replace_all(value, "~$","")) %>% 
+  filter(!grepl("Product",value)) %>% 
    mutate(sheet = map(.$value, excel_sheets)) %>% 
    unnest(cols = c(sheet))
  
@@ -50,6 +53,91 @@ setwd(dimens)
 
 tables %>% 
   openxlsx::write.xlsx(.,"dimensions_unite.xlsx", overwrite = T)
+
+
+
+# Entities --------------------------------------------------------------------
+
+
+regtot <- tables %>% filter(dimension == "Entity") %>% filter(grepl("REG_TOT",structure))
+
+
+#GEM_LEG no REG_TOT Family
+tables %>% filter(dimension == "Entity") %>% filter(grepl("GEM",structure)) %>% filter(member_type == "Parent")
+
+#GEM_EMG_LEG
+tables %>% filter(dimension == "Entity") %>% filter(grepl("GEM_EMG",structure))
+
+#EMG_META_TOT
+tables %>% filter(dimension == "Entity") %>% filter(grepl("EMG_META",structure))
+
+#EMG_MENA_TOT
+tables %>% filter(dimension == "Entity") %>% filter(grepl("EMG_MENA",structure))
+
+#GTS_GEM_ASIA
+tables %>% filter(dimension == "Entity") %>% filter(grepl("GTS_GEM_ASIA",structure))
+
+#EMG_IND_incl_Pune
+tables %>% filter(dimension == "Entity") %>% filter(grepl("EMG_IND",structure))
+
+#ASIA_CHN_TOT
+tables %>% filter(dimension == "Entity") %>% filter(grepl("ASIA_CHN",structure))
+
+
+# Products ----------------------------------------------------------------
+
+# PTG
+tables %>% filter(dimension == "Prod") %>% filter(grepl("PTG",structure))
+
+#HTAS
+tables %>% filter(dimension == "Prod") %>% filter(grepl("HTAS",structure))
+
+#HTS
+tables %>% filter(dimension == "Prod") %>% filter(grepl("HTS",structure))
+
+#OPG
+tables %>% filter(dimension == "Prod") %>% filter(grepl("OPG",structure))
+
+#OutD
+tables %>% filter(dimension == "Prod") %>% filter(grepl("Outdoor",structure))
+
+#CPT 
+tables %>% filter(dimension == "Prod") %>% filter(grepl("CPT",structure))
+
+
+
+# Dimensions Search Engine ------------------------------------------------
+
+
+
+dimens_search <- function(dimen, pattern){
+  
+  tables %>% filter(dimension == dimen) %>% 
+             filter(grepl(pattern,structure))
+  
+}
+
+
+explore <- tibble(dimension = c("Entity","Entity"),
+                  structure = c("ALLOC", "REG_TOT"))
+
+map2(explore$dimension, 
+     explore$structure,
+     dimens_search)
+
+
+tables %>% filter(dimension == "Entity") %>% 
+           filter(!grepl("Adjust", structure)) %>% 
+           filter(!grepl("Alloc",structure)) %>%
+           filter(grepl("REG_TOT",sub_dimension))-> reg_vector
+
+# Value Match
+
+origin <- openxlsx::read.xlsx("../origin.xlsx") %>% as_tibble()
+
+reg_tot <- openxlsx::read.xlsx("../REG_TOT.xlsx") %>% as_tibble()
+
+
 
 
 
